f92cce2c7f182f4afe21ebff2ed26b8a4029f35c
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2020-12-26 00:46:04 +0100

    Use 'export from' instead of writing the same codes

--- pages/home.js
+++ pages/home.js
@@ -1,31 +1,3 @@
-import { useRouter } from 'next/router'
+// Load the same page as index to avoid i18n redirects.
 
-import Layout from 'components/layout'
-import { getContentData } from 'lib/contents'
-
-export default function Content({ postData }) {
-  const { locale } = useRouter()
-  return (
-    <Layout
-      title={postData.title}
-      date={postData.date}
-      isTranslated={postData.isTranslated}
-      description={postData.description}
-      keywords={postData.keywords}
-      eyecatch={postData.eyecatch}
-    >
-      <article>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
-  )
-}
-
-export async function getStaticProps({ params, locale }) {
-  const postData = await getContentData('', 'index', locale)
-  return {
-    props: {
-      postData,
-    },
-  }
-}
+export { default, getStaticProps } from 'pages/index'
