d883f096262e0ca4e55bef7ed0210868f4db0dee
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2023-04-26 08:47:37 +0200

    © 2023

--- components/layout.js
+++ components/layout.js
@@ -148,7 +148,7 @@ export default function Layout({
       <div className="site-footer">
         <div className="wrapper">
           <p>
-            © 2021
+            © 2023
             <Link href="/home" as="/">
               <a> Shoya Ishimaru</a>
             </Link>{' '}
