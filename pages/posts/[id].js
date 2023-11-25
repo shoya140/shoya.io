4f429b1f81119f1414ca47b86113c5660bce5dfd
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2023-11-25 10:34:04 +0900

    Twitter -> X

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -38,7 +38,7 @@ export default function Post({ postData }) {
             )
           }}
         >
-          Share on Twitter
+          Share on X
         </button>
       </div>
     </Layout>
